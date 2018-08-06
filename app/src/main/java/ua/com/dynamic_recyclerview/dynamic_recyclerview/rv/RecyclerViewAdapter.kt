218ea78e9d300288c0cb002c6dc52ab57950c7e3
Perelomov <the.perelomov@gmail.com>
2018-08-06 16:37:28 +0300

    Создал класс TestViewHolder

--- app/src/main/java/ua/com/dynamic_recyclerview/dynamic_recyclerview/rv/RecyclerViewAdapter.kt
+++ app/src/main/java/ua/com/dynamic_recyclerview/dynamic_recyclerview/rv/RecyclerViewAdapter.kt
@@ -1,12 +1,13 @@
 package ua.com.dynamic_recyclerview.dynamic_recyclerview.rv
 
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import android.view.ViewGroup
 
 
-class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.TestViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewAdapter.TestViewHolder {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
@@ -14,11 +15,15 @@ class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerViewAdapter.TestViewHolder, position: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getItemViewType(position: Int): Int {
         return super.getItemViewType(position)
+    }
+
+    class TestViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
     }
 }
